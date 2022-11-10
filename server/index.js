5161896c6187a3b11cdd9dbeabaad03a77c315f3
mollick-portfolio <you@example.com>
2022-11-10 10:28:48 +0600

    update...

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const mongodb = require("mongodb");
 const colors = require("colors");
 const cors = require("cors");
 const dbConnection = require("./server");
+const chats = require("./data");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -11,7 +12,22 @@ app.use(cors());
 app.use(express.json());
 
 // db connection
-// dbConnection()
+dbConnection();
+
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "chatapp home",
+    success: true,
+  });
+});
+
+app.get("/api/v1/chat", async (req, res) => {
+  res.status(200).json({
+    status: true,
+    message: "Data get successfull",
+    data: chats,
+  });
+});
 
 app.listen(port, () => {
   console.log(`app is listen on port ${port}`.bold.blue);
